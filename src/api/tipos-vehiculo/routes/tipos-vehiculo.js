53996077eb0299e866d1fa1f5efbbcfaf3a9de82
Anderson Charry <charry072013@gmail.com>
2024-08-13 17:38:55 -0500

    update strapi v5

--- src/api/tipos-vehiculo/routes/tipos-vehiculo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tipos-vehiculo router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::tipos-vehiculo.tipos-vehiculo');
--- src/api/tipos-vehiculo/services/tipos-vehiculo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tipos-vehiculo service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::tipos-vehiculo.tipos-vehiculo');
